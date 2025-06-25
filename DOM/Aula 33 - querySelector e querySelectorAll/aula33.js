43c7168e2b27c658f017b0b356844c2d0383b244
Sara Weiand Wüst <weiandwustsara@gmail.com>
2025-06-24 21:48:14 -0300

    Aula 34

--- DOM/Aula 33 - querySelector e querySelectorAll/aula33.js
+++ DOM/Aula 33 - querySelector e querySelectorAll/aula33.js
@@ -4,16 +4,17 @@ const cursoC1 = [...document.getElementsByClassName("c1")]
 const cursoC2 = [...document.getElementsByClassName("c2")]
 const cursoEspecial = document.getElementById("c1")
 
-const query_divTodos = document.querySelector("div")
+//const query_divTodos = document.querySelectorAll("div[class]")
+const query_divTodos = document.querySelectorAll("div > p")
 const query_cursoTodos = [...document.querySelectorAll(".curso")]
 const query_cursoC1 = [...document.querySelectorAll(".c1")]
 const query_cursoC2 = [...document.querySelectorAll(".c2")]
 const query_cursoEspecial = document.querySelectorAll("#c1")[0]
 
 console.log(query_divTodos)
-console.log(query_cursoTodos)
-console.log(query_cursoC1)
-console.log(query_cursoC2)
+// console.log(query_cursoTodos)
+// console.log(query_cursoC1)
+// console.log(query_cursoC2)
 
 // console.log(divTodos)
 // console.log(cursoTodos)
---
+++ DOM/Aula 34 - addEventListener/aula34.js
@@ -0,0 +1,18 @@
+//const c1 = document.getElementById("c1")
+const c1 = document.querySelector("#c1")
+
+const msg = () => {
+    alert("clicou")
+}
+c1.addEventListener("click", msg)
+
+
+c1.addEventListener("click",()=>{
+    alert("clicou2")
+    // msg()
+})
+
+
+c1.addEventListener("click", (evt) => {
+    console.log(evt.target)
+})
